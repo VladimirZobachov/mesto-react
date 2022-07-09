c283a75dd471b30d69f5793f04b1b38c6be5f70d
Vladimir <zobachov@gmail.com>
2022-07-09 10:40:32 -0500

    add api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import {useEffect} from "react";
 import api from '../utils/api';
 
 function Main(props) {
@@ -7,21 +8,30 @@ function Main(props) {
     const [userDescription, setUserDescription] = useState('');
     const [userAvatar, setUserAvatar] = useState('');
 
+    useEffect(()=>{
+        api.getUser()
+            .then((res)=>{
+                setUserName(res.name);
+                setUserDescription(res.about);
+                setUserAvatar(res.avatar);
+            })
+    })
+
     return (
         <>
             <main className="content">
                 <section className="profile">
-                    <div className="profile__avatar">
+                    <div className="profile__avatar" style={{ backgroundImage: `url(${userAvatar})` }} >
                         <div className="profile__avatar-edit">
                             <i className="profile__avatar-edit-icon" onClick={props.onEditAvatar}></i>
                         </div>
                     </div>
                     <div className="profile__content">
                         <div className="profile__name">
-                            <h1 className="profile__title"></h1>
+                            <h1 className="profile__title">{userName}</h1>
                             <button aria-label="Edit" type="button" className="profile__edit-button" onClick={props.onEditProfile}></button>
                         </div>
-                        <p className="profile__subtitle"></p>
+                        <p className="profile__subtitle">{userDescription}</p>
                     </div>
                     <button aria-label="Add" type="button" className="profile__add-button" onClick={props.onAddPlace}></button>
                 </section>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,6 @@
-const options =
-{
+const options = {
     baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-43',
-        headers: {
+    headers: {
         authorization:
         '661151b8-675b-4416-83fc-3d0035514aa7',
         'Content-Type': 'application/json'
